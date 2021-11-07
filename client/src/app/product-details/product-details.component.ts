6387a05a830207630b65c27c14943fe1bae53312
rounaka-hexaware <rounaka@hexaware.com>
2021-11-07 16:28:30 +0530

    UI changes

--- client/src/app/product-details/product-details.component.ts
+++ client/src/app/product-details/product-details.component.ts
@@ -1,6 +1,9 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import * as test from '../products-card/test.json'
+import * as test from '../products-card/test.json';
+import * as variants from './variants.json'
+
+
 
 @Component({
   selector: 'app-product-details',
@@ -9,13 +12,30 @@ import * as test from '../products-card/test.json'
 })
 export class ProductDetailsComponent implements OnInit {
 
-  data :any =  test.value[0];
+  variants = variants.value;
+  data :any = variants.value[1];
+  modelViewer = document.querySelector('#model-viewer-id #controls');
+  
   constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    
     const routeParams = this.route.snapshot.paramMap;
   const productIdFromRoute = String(routeParams.get('productId'));
   console.log(productIdFromRoute);
+
+  console.log(document.getElementById("src"))
   }
+  onSelect(value){
+    console.log(value)
+    document.getElementById("model-viewer-id").setAttribute("src" , value)
+    for(let i=0;i<variants.value.length;i++){
+      if(variants.value[i].Src == value){
+        this.data = variants.value[i];
+      }
+      
+    }
+  }
+  
 
 }
